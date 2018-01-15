f63e0e66cf2f0905dcb088cbcc01daf104adff5e
Clinton Mead <clintonmead@gmail.com>
2018-01-15 13:07:20 +1100

    Renamed clashing variable names

--- src/Web/SimpleStaticServer.hs
+++ src/Web/SimpleStaticServer.hs
@@ -25,7 +25,7 @@ runServer :: Port -> FilePath -> IO ()
 runServer port filepath = run port (app filepath)
 
 app :: FilePath -> Request -> (Response -> IO ResponseReceived) -> IO ResponseReceived
-app basedir req f = resp >>= f where
+app basedir req respF = resp >>= respF where
   resp =
     let
       method = requestMethod req
@@ -46,27 +46,27 @@ app basedir req f = resp >>= f where
 
       checkExists :: (PathInfo -> IO Response) -> IO Response
       checkExists f = checkValid go where
-        go pathInfo = do
-          let fullPath = getFullPath pathInfo
-          let fileName = getFileName pathInfo
+        go filePathInfo = do
+          let fullPath = getFullPath filePathInfo
+          let fileName = getFileName filePathInfo
           fileExists <- doesFileExist fullPath
           case fileExists of
-            True -> f pathInfo
+            True -> f filePathInfo
             False -> pure $ responseLBS notFound404 [] ("File: \"" <> LazyByteString.pack fileName <> "\" not found.")
       handleHead = checkExists go where
         go _ = pure $ responseLBS ok200 [] ""
       handleGet = checkExists go where
-        go pathInfo = pure $ responseFile ok200 [] (getFullPath pathInfo) Nothing
+        go filePathInfo = pure $ responseFile ok200 [] (getFullPath filePathInfo) Nothing
       handleDelete = checkExists go where
-        go pathInfo = do
-          removeFile (getFullPath pathInfo)
-          pure $ responseLBS ok200 [] ("File: \"" <> LazyByteString.pack (getFileName pathInfo) <> "\" deleted.")
+        go filePathInfo = do
+          removeFile (getFullPath filePathInfo)
+          pure $ responseLBS ok200 [] ("File: \"" <> LazyByteString.pack (getFileName filePathInfo) <> "\" deleted.")
       handlePut = checkValid go where
-        go pathInfo = do
+        go filePathInfo = do
           body <- strictRequestBody req
-          atomicReplaceFile Nothing (getFullPath pathInfo) body
-          pure $ responseLBS ok200 [] ("File: \"" <> LazyByteString.pack (getFileName pathInfo) <> "\" written.")
-      handlePost = handleUnknown
+          atomicReplaceFile Nothing (getFullPath filePathInfo) body
+          pure $ responseLBS ok200 [] ("File: \"" <> LazyByteString.pack (getFileName filePathInfo) <> "\" written.")
+      handlePost = handlePut
       handleUnknown = pure $ responseLBS methodNotAllowed405 [] ("Unknown method " <> LazyByteString.fromStrict method)
     in if
       | eqMethod methodHead   -> handleHead
